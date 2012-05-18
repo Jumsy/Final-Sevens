9c634901188b680c99f5f27c4676a9f4f23e5b59
James Huyck <james.huyck@gmail.com>
2012-05-18 13:24:06 -0400

    Removed curBlock and put the blocks straight into botBlocks

--- Final-Sevens.py
+++ Final-Sevens.py
@@ -16,6 +16,9 @@ pygame.display.set_caption('Final Sevens')
 screen = pygame.display.get_surface()
 
 BLOCKSIZE = 40
+MOVESPEED = BLOCKSIZE / 10
+ROWS = 13
+COLS = 7
 
 mainFont = pygame.font.SysFont(None, 19) #Pause menu text
 blockFont = pygame.font.SysFont(None, 40) #Block numbers and pause menu score
@@ -34,7 +37,7 @@ class gd: #Global data
              ]
 
     #A copy of the colors list for resetting it back to normal when changed
-    normalColors = list(colors) # normalColors = colors[:] would also work
+    normalColors = list(colors)
 
 def pause_screen(score):
     '''
@@ -101,16 +104,14 @@ def pause_screen(score):
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == ord('r'):
+                    resetStr = "RESET IS ACTIVE!"
                     if reset:
                         reset = False
-                        resetStr = "RESET IS ACTIVE!"
                         resetText = mainFont.render(resetStr, 1, bgcolor)
-                        screen.blit(resetText, (WINDOWWIDTH/3, incrementy - 15))
                     else:
                         reset = True
-                        resetStr = "RESET IS ACTIVE!"
                         resetText = mainFont.render(resetStr, 1, (255, 0, 0))
-                        screen.blit(resetText, (WINDOWWIDTH/3, incrementy - 15))
+                    screen.blit(resetText, (WINDOWWIDTH/3, incrementy - 15))
                 elif event.key in [ord('j'), ord('J')]:
                     gd.colors = list(gd.normalColors)
                     print_pause_blocks(incrementy)
@@ -152,14 +153,12 @@ def get_block_num():
     else:
         return 7
 
-def spawn_blocks(blockList, curBlock):
-    """Spawns new blocks in topBlocks and gives blocks nudges
-    to the next level of play (topBlocks -> spawning area -> curBlock)
-    """
+def spawn_blocks(topBlocks, botBlocks):
     windowCenter = WINDOWWIDTH / 2
+    curBlockCol = int(COLS/2)
 
     for i in [0, -1]: #0: top left, -1: top right
-        block = blockList[i]
+        block = topBlocks[i]
         if block[0] == 0:
             new_number = get_block_num()
             block[0] = new_number
@@ -170,25 +169,26 @@ def spawn_blocks(blockList, curBlock):
             if i: block[3] = 'left'
             else: block[3] = 'right'
 
-    if blockList[3][0] == 0 and (blockList[2][0] or blockList[4][0]):
+    if topBlocks[3][0] == 0 and (topBlocks[2][0] or topBlocks[4][0]):
         order = [2, 4]
         random.shuffle(order)
         for i in order:
-            if blockList[i][0]:
-                if blockList[i][1].centerx < windowCenter:
-                    blockList[i][1].centerx += 4
-                elif blockList[i][1].centerx > windowCenter:
-                    blockList[i][1].centerx -= 4
-                blockList[3] = blockList[i][:]
-                blockList[i][0] = 0
+            if topBlocks[i][0]:
+                if topBlocks[i][1].centerx < windowCenter:
+                    topBlocks[i][1].centerx += 4
+                elif topBlocks[i][1].centerx > windowCenter:
+                    topBlocks[i][1].centerx -= 4
+                topBlocks[3] = topBlocks[i][:]
+                topBlocks[i][0] = 0
                 break
 
-    if blockList[3][0] and not curBlock[0]:
-        if blockList[3][1].centerx == windowCenter:
-            curBlock = list(blockList[3])
-            blockList[3][0] = 0
+    if topBlocks[3][0] and not botBlocks[curBlockCol][0][0]:
+        if topBlocks[3][1].centerx == windowCenter:
+            topBlocks[3][3] = 'down'
+            botBlocks[curBlockCol][0] = list(topBlocks[3])
+            topBlocks[3][0] = 0
     
-    return blockList, curBlock
+    return topBlocks, botBlocks
 
 def print_block(block):
     if block[0] == 0: return
@@ -201,15 +201,15 @@ def print_block(block):
 
 def check_overlap(loc, blocks):
     for block in blocks:
-        if block[0] == 0: continue
-        if loc == [block[1].centerx, block[1].centery]:
+        if block[0] != 0 and loc == [block[1].centerx, block[1].centery]:
             return True
     return False
 
 def move_blocks(blocks, btype):
     windowCenter = WINDOWWIDTH / 2
     topStops = [windowCenter, windowCenter-BLOCKSIZE, windowCenter+BLOCKSIZE]
-    directionMap = {'left': [-4,0], 'right': [4,0], 'up':[0,-4], 'down':[0,4]}
+    directionMap = {'left': [-MOVESPEED,0], 'right': [MOVESPEED,0],
+                             'up':[0,-MOVESPEED], 'down':[0,MOVESPEED]}
 
     for block in blocks:
         if block[0] == 0 or (btype == 'top' and block[1].centerx in topStops):
@@ -219,7 +219,11 @@ def move_blocks(blocks, btype):
         targetx = block[1].centerx + directionMap[block[3]][0] * 10
         targety = block[1].centery + directionMap[block[3]][1] * 10
 
-        if check_overlap([targetx, targety], blocks):
+        bottom = block[1].bottom
+        if check_overlap([targetx, targety], blocks) or bottom == WINDOWHEIGHT:
+            if btype == 'bot':
+                blocks[(block[1].top / BLOCKSIZE) - 1] = block[:]
+                block[0] = 0
             continue
 
         block[1].centerx += directionMap[block[3]][0]
@@ -231,25 +235,22 @@ def move_blocks(blocks, btype):
 
     return blocks
 
-def setup_blocks(rows, cols):
+def setup_blocks(cols, rows):
     #Block format [number, rect, number rect, direction]
-    blockt = [0, None, None, None] #Block template
-    topBlocks = [ blockt[:] for i in range(cols) ]
-    curBlock = blockt[:]
-    fallenBlocks = [ [ blockt[:] for i in range(cols) ] for j in range(rows) ]
+    blockf = [0, None, None, None]
+    topBlocks = [ blockf[:] for i in range(cols) ]
+    botBlocks = [ [ blockf[:] for i in range(cols) ] for j in range(rows) ]
     
-    return topBlocks, curBlock, fallenBlocks
+    return topBlocks, botBlocks
 
 def main():
-    moveSpeed = 4
     moveSide = 0
     score = 0
     gameSpeed = 40
     pause_screen(score)
 
-    topBlocks, curBlock, fallenBlocks = setup_blocks(13, 7)
+    topBlocks, botBlocks = setup_blocks(ROWS, COLS)
 
-    
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -264,12 +265,13 @@ def main():
                     gameSpeed = 120
                 elif event.key in [ord('p'), ord('P')]:
                     if pause_screen(score):
-                        topBlocks, curBlock, fallenBlocks = setup_blocks(13, 7)
+                        topBlocks, botBlocks = setup_blocks(ROWS, COLS)
                         score = 0
             elif event.type == KEYUP:
                 if event.key in [K_DOWN, ord('s'), ord('S')]:
                     gameSpeed = 40
 
+        #This draws the background
         window.fill(gd.colors[0])
         for i in range(0, 280, 40):
             pygame.draw.line(window, gd.colors[8], (i,40), (i,WINDOWHEIGHT), 1)
@@ -277,20 +279,24 @@ def main():
         pygame.draw.line(window, gd.colors[8], (160, 40), (280, 40), 1)
 
         #Spawn new blocks
-        topBlocks, curBlock = spawn_blocks(topBlocks, curBlock)
+        topBlocks, botBlocks = spawn_blocks(topBlocks, botBlocks)
 
         topBlocks = move_blocks(topBlocks, 'top')
+        for col in botBlocks:
+            col = move_blocks(col, 'bot')
 
-        #Print all the blocks
-        allBlocks = topBlocks + [curBlock]
-        allBlocks.extend(block for line in fallenBlocks for block in line)
+        #Add all the blocks to a single list which is passed to a print function
+        allBlocks = topBlocks[:]
+        allBlocks.extend(block for line in botBlocks for block in line)
         map(print_block, allBlocks)
         pygame.display.update()
 
-        if fallenBlocks[3][0][0] != 0: break #Game over
+        if botBlocks[3][0][0] != 0 and botBlocks[3][1][0]:
+            break #Game over
 
         clock.tick(gameSpeed)
 
     print("Thanks for playing!")
+    print("Your score: %s" %(score)) 
 
 main()
